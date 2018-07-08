4e433b9c3d15b4c9155cbbd9cc116c72e7c4cd96
Edsel Caprice <caprice@mail.usf.edu>
2018-07-08 01:01:44 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,10 @@ def execute_transaction
  @sender.balance -= @amount
  @receiver.balance += @amount
  @status = "complete"
-end
+ elsif !sender.valid?
+   "Transaction rejected. Please check your account balance."
+   
+ end
 end
 
 end
