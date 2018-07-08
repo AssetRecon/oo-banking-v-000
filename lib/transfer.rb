f4f38288b3a4e74b964d22096963a576962911cb
Edsel Caprice <caprice@mail.usf.edu>
2018-07-08 00:59:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,11 @@ def valid?
 end
 
 def execute_transaction
-  if @status = "pending"
+  if @status == "pending"
  @sender.balance -= @amount
  @receiver.balance += @amount
  @status = "complete"
- elsif @status = "complete"
-   @sender.balance
-   @receiver.balance
-  end
+
 end
 
 end
